e706e22cb403df35d3f306796551b54fbd6bb4ee
FhilF <52390799+FhilF@users.noreply.github.com>
2023-03-30 11:43:47 +0800

    added infinite scroll feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,79 @@ import "./App.scss";
 function App() {
   const [launches, setLaunches] = useState([]);
   const [isPageLoading, setIsPageLoading] = useState(true);
-  
-  useEffect(() => {
-    setIsPageLoading(true)
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+
+  const noOfItems = 15;
+
+  const fetchData = (page) => {
+    const newItems = [];
     axios
       .get("https://api.spacexdata.com/v3/launches")
       .then((res) => {
-        setLaunches(res.data);
-        setIsPageLoading(false)
+        const items = res.data;
+        for (
+          let i = launches.length;
+          i <
+          (items.length > launches.length + noOfItems
+            ? launches.length + noOfItems
+            : items.length);
+          i++
+        ) {
+          newItems.push(items[i]);
+        }
+        const newLaunches = [...launches, ...newItems];
+        if (items.length > newLaunches.length) {
+          setHasMore(true);
+        } else {
+          setHasMore(false);
+        }
+        setLaunches(newLaunches);
+        setIsPageLoading(false);
       })
       .catch((err) => {
-        setIsPageLoading(false)
+        setIsPageLoading(false);
       });
-  }, []);
+
+    // setItems([...items, ...newItems]);
+  };
+
+  // useEffect(() => {
+  //   setIsPageLoading(true);
+  //   axios
+  //     .get("https://api.spacexdata.com/v3/launches")
+  //     .then((res) => {
+  //       setLaunches(res.data);
+  //       setIsPageLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       setIsPageLoading(false);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log(launches);
+  }, [launches]);
+
+  useEffect(() => {
+    if (hasMore) {
+      fetchData();
+    }
+  }, [page]);
+
+  const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight) {
+      setPage(page + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [launches]);
 
   return (
     <Box className="app">
